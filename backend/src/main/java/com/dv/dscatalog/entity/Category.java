c5e1c4de1f1a425843ac6e51ef7764492ce21097
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-05-27 00:43:40 -0300

    H2 database, category service, category repository and fix category entity

--- backend/src/main/java/com/dv/dscatalog/entity/Category.java
+++ backend/src/main/java/com/dv/dscatalog/entity/Category.java
@@ -2,12 +2,20 @@ package com.dv.dscatalog.entity;
 
 import java.util.Objects;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	
 	public Category() {
