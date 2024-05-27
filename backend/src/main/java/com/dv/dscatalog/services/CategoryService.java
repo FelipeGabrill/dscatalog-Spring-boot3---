9f2cb4de3df62feb29215b7393d4b443e5df46e2
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-05-27 20:50:41 -0300

    Post Category

--- backend/src/main/java/com/dv/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dv/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dv.dscatalog.dto.CategoryDTO;
 import com.dv.dscatalog.entity.Category;
@@ -17,15 +18,25 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Category category = repository.findById(id).orElseThrow(
 				() -> new EntityNotFoundException("Entity not found"));
 		return new CategoryDTO(category);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
 	}
 	
 	
