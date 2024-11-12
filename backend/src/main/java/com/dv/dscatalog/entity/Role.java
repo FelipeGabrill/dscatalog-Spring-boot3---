4c2e643f5007ab239e60a1561439072dd8c30673
FelipeGabrill <felipegabrielodss@gmail.com>
2024-11-12 20:30:42 -0300

    Mapeamento objeto-relacional, seed do banco

--- backend/src/main/java/com/dv/dscatalog/entity/Role.java
+++ backend/src/main/java/com/dv/dscatalog/entity/Role.java
@@ -2,10 +2,14 @@ package com.dv.dscatalog.entity;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_role")
 public class Role {
 
 	@Id
