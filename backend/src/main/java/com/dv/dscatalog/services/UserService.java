8a36490acdc83edcf149a037f52ebc332f49a758
FelipeGabrill <felipegabrielodss@gmail.com>
2024-11-14 03:15:34 -0300

    UserDetailsService

--- backend/src/main/java/com/dv/dscatalog/services/UserService.java
+++ backend/src/main/java/com/dv/dscatalog/services/UserService.java
@@ -1,10 +1,15 @@
 package com.dv.dscatalog.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +20,7 @@ import com.dv.dscatalog.dto.UserInsertDTO;
 import com.dv.dscatalog.dto.UserUpdateDTO;
 import com.dv.dscatalog.entity.Role;
 import com.dv.dscatalog.entity.User;
+import com.dv.dscatalog.projections.UserDetailsProjection;
 import com.dv.dscatalog.repositories.RoleRepository;
 import com.dv.dscatalog.repositories.UserRepository;
 import com.dv.dscatalog.services.exceptions.DatabaseException;
@@ -23,10 +29,10 @@ import com.dv.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private UserRepository repository;
@@ -91,5 +97,22 @@ public class UserService {
 			Role role = roleRepository.getReferenceById(roleDto.getId());
 			entity.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		if (result.size() == 0) {
+			throw new UsernameNotFoundException("Email não encontrado");
+		}
+		
+		User user = new User();
+		user.setEmail(result.get(0).getUsername());
+		user.setPassword(result.get(0).getPassword());
+		for (UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		
+		return user;
 	}
 }
